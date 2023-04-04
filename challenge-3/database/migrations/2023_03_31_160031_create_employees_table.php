87ef2f56c9d11eed3699dd3e90a7ccdf349cf0ce
Saad <saadrazi14@gmail.com>
2023-04-04 15:53:17 +0500

    Challenge 3 migrations

---
+++ challenge-3/database/migrations/2023_03_31_160031_create_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->foreignId('company_group_id')->constrained('company_groups')->nullable();
+            $table->foreignId('people_id')->constrained('peoples');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
